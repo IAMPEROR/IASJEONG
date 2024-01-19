2546c99325446fd29b51f28581b902e5b8d9d588
IAMPEROR <156991840+IAMPEROR@users.noreply.github.com>
2024-01-19 17:12:39 +0900

    Add files via upload

---
+++ server.js
@@ -0,0 +1,21 @@
+// npm init
+// npm install express
+// npm install -g nodemon -> nodemon server.js -> powershell : set-executionpolicy unrestricted
+
+
+const express = require('express');
+const app = express();
+
+app.listen(7770, function(){
+
+    console.log('listening on 7770')
+
+});
+
+app.get('/hello', function(요청, 응답){
+    응답.send('Hello');
+});
+
+app.get('/', function(요청, 응답){
+    응답.sendFile(__dirname + '/index.html');
+});
