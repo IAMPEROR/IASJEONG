6edfd11a9ad2debf03c2bd02188a7368455e9836
IAMPEROR <156991840+IAMPEROR@users.noreply.github.com>
2024-01-22 15:49:27 +0900

    first commit

--- server.js
+++ server.js
@@ -7,15 +7,9 @@ const express = require('express');
 const app = express();
 
 app.listen(7770, function(){
-
     console.log('listening on 7770')
-
-});
-
-app.get('/hello', function(요청, 응답){
-    응답.send('Hello');
 });
 
 app.get('/', function(요청, 응답){
-    응답.sendFile(__dirname + '/index.html');
+    응답.sendFile(__dirname + '/index.html')
 });
